f270097f490457a676f1ddb9beb0c1ab26abe238
LesserCold <gulhanguang@gmail.com>
2022-11-25 14:17:33 +0900

    first

---
+++ src/Components/Sidebar/index.js
@@ -0,0 +1,95 @@
+import React, { Component, useState } from 'react'
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Drawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 240;
+export default class index extends Component {
+
+
+    render() {
+
+
+
+        const buttons = [
+            <Button key="one">One</Button>,
+            <Button key="two">Two</Button>,
+            <Button key="three">Three</Button>,
+        ];
+
+
+        return (
+            <div>
+                    <CssBaseline />
+                    <AppBar
+                        position="fixed"
+                        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+                    >
+                        <Toolbar>
+                            <Typography variant="h6" noWrap component="div">
+                                Calendar
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                    <Drawer
+                        sx={{
+                            width: drawerWidth,
+                            flexShrink: 0,
+                            '& .MuiDrawer-paper': {
+                                width: drawerWidth,
+                                boxSizing: 'border-box',
+                            },
+                        }}
+                        variant="permanent"
+                        anchor="left"
+                    >
+                        <Toolbar />
+                        <Divider />
+                        <List>
+                            {['1', '2', '3', '4'].map((text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                        <Divider />
+                        <List>
+                            {['Calender1', 'Calender2', 'Calender3'].map((text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Drawer>
+          
+
+            </div>
+
+
+
+        )
+    }
+}
