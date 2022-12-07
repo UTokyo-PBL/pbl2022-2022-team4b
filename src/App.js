e67b424ca070fbc0b00faa15cadda9ac94938557
Kintorch <gulhanguang@gmail.com>
2022-12-07 13:08:47 +0900

    侧边栏

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
-import { Scheduler } from "@aldabil/react-scheduler";
-import ja from 'date-fns/locale/ja'
 
-
+import React, { Component } from 'react'
 import Sidebar from "./Components/Sidebar"
+import Calendar from "./Components/Calendar"
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
 
+  state = {calendarNames:["calendar1", "calendar2"], scheules:{}}
 
-      <Box sx={{ display: 'flex' }}>
+  render() {
 
-        <Sidebar />
+    const {calendarNames, scheules} = this.state
 
-        <Box
-          component="main"
-          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-        >
-           <Toolbar />
-          <Scheduler
-            locale={ja}
-            view="month"
-            events={[
-              {
-                event_id: 1,
-                title: "Event 1",
-                start: new Date("2022/5/2 09:30"),
-                end: new Date("2022/5/2 10:30"),
-              },
-              {
-                event_id: 2,
-                title: "Event 2",
-                start: new Date("2022/5/4 10:00"),
-                end: new Date("2022/5/4 11:00"),
-              },
-            ]}
-          />
+    return (
+
+      <div className="App">
+        <Box sx={{ display: 'flex' }}>
+
+          <Sidebar calendarNames={calendarNames}/>
+
+          <Calendar scheules={scheules}/>
+
 
         </Box>
-
-
-
-      </Box>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,8 +1,6 @@
-import React, { Component, useState } from 'react'
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+import React, { Component, useState, setState } from 'react'
+import { styled, useTheme } from '@mui/material/styles';
 import Drawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -14,82 +12,148 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import Box from '@mui/material/Box';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import PubSub from 'pubsub-js'
 
-const drawerWidth = 240;
+
+
+export const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
+
+
 export default class index extends Component {
 
 
+    state = { drawerOpen: false }
+
+
+    toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        this.setState({ drawerOpen: open });
+
+        PubSub.publish('drawerOpen', { drawerOpen: open })
+    };
+
+
+    calendarSelected = (id) => {
+
+    }
+
+    addNewCalendar = () => {
+        
+    }
+
+
     render() {
-
-
-
-        const buttons = [
-            <Button key="one">One</Button>,
-            <Button key="two">Two</Button>,
-            <Button key="three">Three</Button>,
-        ];
-
-
+        console.log(this.props)
+        const { drawerOpen } = this.state
+        const { calendarNames } = this.props
         return (
-            <div>
-                    <CssBaseline />
-                    <AppBar
-                        position="fixed"
-                        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-                    >
-                        <Toolbar>
-                            <Typography variant="h6" noWrap component="div">
-                                Calendar
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>
-                    <Drawer
-                        sx={{
-                            width: drawerWidth,
-                            flexShrink: 0,
-                            '& .MuiDrawer-paper': {
-                                width: drawerWidth,
-                                boxSizing: 'border-box',
-                            },
-                        }}
-                        variant="permanent"
-                        anchor="left"
-                    >
-                        <Toolbar />
-                        <Divider />
-                        <List>
-                            {['1', '2', '3', '4'].map((text, index) => (
-                                <ListItem key={text} disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                        </ListItemIcon>
-                                        <ListItemText primary={text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                        <Divider />
-                        <List>
-                            {['Calender1', 'Calender2', 'Calender3'].map((text, index) => (
-                                <ListItem key={text} disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                        </ListItemIcon>
-                                        <ListItemText primary={text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Drawer>
-          
+            <>``
 
-            </div>
+                <CssBaseline />
+
+                <AppBar position="fixed" open={this.state['drawerOpen']}>
+
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={this.toggleDrawer(true)}
+                            edge="start"
+                            sx={{ mr: 2, ...(this.drawerOpen && { display: 'none' }) }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+
+                        <Typography variant="h6" noWrap component="div">
+                            Calendar
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
+                <Drawer
+                    sx={{
+                        width: this.drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: this.drawerWidth,
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                    open={this.state['drawerOpen']}
+                    variant='persistent'
+                    anchor="left"
+                >
+                    <DrawerHeader>
+                        <IconButton onClick={this.toggleDrawer(false)}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </DrawerHeader>
+
+                    <Divider />
+                    <List>
+                        {['1', '2', '3', '4'].map((text) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <InboxIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {calendarNames.map((text) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton onClick={this.calendarSelected}>
+                                    <ListItemIcon>
+                                        <CalendarMonthIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+
+
+                    <List style={{ marginTop: `auto` }}>
+
+                        <ListItem disablePadding>
+                            <ListItemButton onClick={this.addNewCalendar}>
+                                <ListItemIcon>
+                                    <CalendarMonthIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={'Add new calendar'} />
+                            </ListItemButton>
+                        </ListItem>
+
+                    </List>
 
 
 
+                </Drawer>
+
+
+
+
+
+            </>
         )
     }
 }
