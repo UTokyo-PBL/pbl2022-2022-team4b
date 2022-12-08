c675a04287c690735564858d99455b0f9f5f8837
Kintorch <gulhanguang@gmail.com>
2022-12-08 19:48:17 +0900

    更换成函数式组件

--- src/App.js
+++ src/App.js
@@ -3,28 +3,31 @@ import React, { Component } from 'react'
 import Sidebar from "./Components/Sidebar"
 import Calendar from "./Components/Calendar"
 import Box from '@mui/material/Box';
+import { Routes, Route } from 'react-router-dom';
 
-export default class App extends Component {
-
-  state = {calendarNames:["calendar1", "calendar2"], scheules:{}}
-
-  render() {
-
-    const {calendarNames, scheules} = this.state
-
-    return (
-
-      <div className="App">
-        <Box sx={{ display: 'flex' }}>
-
-          <Sidebar calendarNames={calendarNames}/>
-
-          <Calendar scheules={scheules}/>
+function App() {
 
 
-        </Box>
-      </div>
-    )
-  }
+
+  [state, setState] = useState({ calendarNames: ["calendar1", "calendar2"], scheules: {} })
+
+
+  const { calendarNames, scheules } = state
+
+  return (
+
+    <div className="App">
+      <Box sx={{ display: 'flex' }}>
+
+        <Sidebar calendarNames={calendarNames} />
+
+        <Calendar scheules={scheules} />
+
+
+      </Box>
+    </div>
+  )
+
 }
 
+export default App
